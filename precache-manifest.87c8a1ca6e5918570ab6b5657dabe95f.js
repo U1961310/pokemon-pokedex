91b1c672152bc80c0833cd3705438e6478bca4c3
U1961310 <u1961310@unimail.hud.ac.uk>
2022-04-27 15:49:18 +0100

    Updates

---
+++ precache-manifest.87c8a1ca6e5918570ab6b5657dabe95f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e467d2d31fe5589e0da4",
+    "url": "/pokemon-pokedex/static/css/main.f2c47cab.chunk.css"
+  },
+  {
+    "revision": "e467d2d31fe5589e0da4",
+    "url": "/pokemon-pokedex/static/js/main.e467d2d3.chunk.js"
+  },
+  {
+    "revision": "266190d1fb51ad12d0a4",
+    "url": "/pokemon-pokedex/static/js/1.266190d1.chunk.js"
+  },
+  {
+    "revision": "bdddd22181b28465e501",
+    "url": "/pokemon-pokedex/static/css/2.258b3e96.chunk.css"
+  },
+  {
+    "revision": "bdddd22181b28465e501",
+    "url": "/pokemon-pokedex/static/js/2.bdddd221.chunk.js"
+  },
+  {
+    "revision": "25ff9cd981ae8921b468",
+    "url": "/pokemon-pokedex/static/js/runtime~main.25ff9cd9.js"
+  },
+  {
+    "revision": "e1304e296ffe11527b92e1a872520371",
+    "url": "/pokemon-pokedex/index.html"
+  }
+];
